76620904df259b0758845739db98828dff797e22
Jeffrey Brown <jeffreybrown7193@gmail.com>
2020-08-11 15:42:30 -0400

    Vien Merge

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { getThemeColor, setThemeColor } from './app/utils/util';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const color =
+  environment.isMultiColorActive || environment.isDarkSwitchActive
+    ? getThemeColor()
+    : environment.defaultColor;
+
+import('./assets/css/sass/themes/vien.' + color + '.scss')
+  .then((x) => {
+    setThemeColor(color);
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+  })
+  .catch(() => {
+    setThemeColor(null);
+    window.location.reload();
+  });
