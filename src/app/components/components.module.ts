f49ef179a2b0ac8fed651e9d7fb7dc58e6335d10
Jeffrey Brown <jeffreybrown7193@gmail.com>
2020-08-12 21:28:24 -0400

    projects-item-component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectItemComponent } from './project-item/project-item.component';
+
+@NgModule({
+  declarations: [ProjectItemComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProjectItemComponent
+  ]
+})
+export class ComponentsModule { }
