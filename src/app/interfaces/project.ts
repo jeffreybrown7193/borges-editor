12fdae570160b677a169238ba18b2fbd61df8299
Jeffrey Allan Brown <jeffreybrown7193@gmail.com>
2020-08-17 09:02:28 -0400

    project service | studio sidebar

---
+++ src/app/interfaces/project.ts
@@ -0,0 +1,15 @@
+export interface Project {
+  _id: Number,
+  user: String,
+  name: String,
+  type: String,
+  description: String,
+  status: String,
+  tags: String,
+  parts: String,
+  chapters: String,
+  characters: String,
+  places: String,
+  created: Date,
+  updated: Date
+}
